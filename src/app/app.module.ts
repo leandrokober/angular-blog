e89d4db6709d551ddbde315f229efe945613c775
Leandro Kober <116602214+leandrokober@users.noreply.github.com>
2023-12-04 11:17:20 -0300

    Criação componentes da página inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuBarComponent } from './menu-bar/menu-bar.component';
-import { BigCardComponent } from './big-card/big-card.component';
-import { MenuTitleComponent } from './menu-title/menu-title.component';
-import { SmallCardComponent } from './small-card/small-card.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SmallCardComponent } from './small-card/small-card.component';
     MenuBarComponent,
     BigCardComponent,
     MenuTitleComponent,
-    SmallCardComponent
+    SmallCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
